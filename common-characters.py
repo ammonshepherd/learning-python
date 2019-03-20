ac9e56ac7cd7b8c59ae1116ab94fd6b7a7523499
Ammon Shepherd <ammon.uva@gmail.com>
2019-03-20 08:43:16 -0400

    Adds module to count the common characters in two strings.

---
+++ common-characters.py
@@ -0,0 +1,25 @@
+# Exercise 8.2 Write a function that gets as parameters two strings. The
+# function returns the number of characters that the strings have in common.
+# Each character counts only once, e.g., the strings "bee" and "peer" only have
+# one character in common (the letter “e”). You can consider capitals different
+# from lower case letters. Note: the function should return the number of
+# characters that the strings have in common, and not print it. To test the
+# function, you can print the result in your main program.
+
+def common_characters(string1, string2):
+    count = 0
+    same = ''
+    for x in string1:
+        for z in string2:
+            if x == z:
+                if z not in same:
+                    same += z
+                    count += 1
+    return count
+
+if __name__ == '__main__':
+    word1 = 'gopher'
+    word2 = 'golfball'
+    num = common_characters(word1, word2)
+    print("The answer should be 2.")
+    print("There are {} common letters in the words ({}) and ({}).".format(num, word1, word2))
