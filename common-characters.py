f81a67c96eb2e319bf8d652256e4e23bbecadc01
Ammon Shepherd <ammon.uva@gmail.com>
2020-06-16 10:59:01 -0400

    some changes

--- common-characters.py
+++ common-characters.py
@@ -17,9 +17,17 @@ def common_characters(string1, string2):
                     count += 1
     return count
 
+def common_letters(word1, word2):
+    set1 = set(word1)
+    set2 = set(word2)
+    same = set1 & set2
+
+    return len(same)
+
 if __name__ == '__main__':
     word1 = 'gopher'
     word2 = 'golfball'
-    num = common_characters(word1, word2)
+    #num = common_characters(word1, word2)
+    num = common_letters(word1, word2)
     print("The answer should be 2.")
     print("There are {} common letters in the words ({}) and ({}).".format(num, word1, word2))
